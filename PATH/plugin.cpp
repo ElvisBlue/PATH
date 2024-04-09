6b44f9ca5b57a491b35f1de9631833c34b59c46c
ElvisBlue <elvis200070@gmail.com>
2024-04-09 11:47:19 +0700

    Finish basic plugin code

--- PATH/plugin.cpp
+++ PATH/plugin.cpp
@@ -1,4 +1,5 @@
 #include "plugin.h"
+#include <Windows.h>
 
 // Examples: https://github.com/x64dbg/x64dbg/wiki/Plugins
 // References:
@@ -7,11 +8,65 @@
 // - https://x64dbg.com/blog/2016/10/20/threading-model.html
 // - https://x64dbg.com/blog/2016/07/30/x64dbg-plugin-sdk.html
 
+BOOL AddDllDirectoryWithEnv(wchar_t* path)
+{
+    wchar_t pathContent[4096];
+    if (!ExpandEnvironmentStrings(path, pathContent, sizeof(pathContent) / sizeof(wchar_t)))
+    {
+        return FALSE;
+    }
+    return (AddDllDirectory(pathContent) != 0);
+}
+
 // Initialize your plugin data here.
 bool pluginInit(PLUG_INITSTRUCT* initStruct)
 {
-    SetDllDirectory(L"asdf");
-    // Return false to cancel loading the plugin.
+    HKEY hKey;
+    LONG lResult;
+
+    // ChatGPT....
+    // Open the registry key
+    lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", 0, KEY_READ, &hKey);
+
+    if (lResult != ERROR_SUCCESS) {
+        _plugin_logprint("RegOpenKeyEx failed\n");
+        return false;
+    }
+
+    const DWORD bufferSize = 2048;
+    wchar_t buffer[bufferSize];
+    DWORD dataSize = bufferSize;
+
+    // Read the registry value
+    lResult = RegQueryValueEx(hKey, L"Path", NULL, NULL, reinterpret_cast<LPBYTE>(buffer), &dataSize);
+
+    if (lResult != ERROR_SUCCESS) {
+        _plugin_logprint("Error reading registry value\n");
+        RegCloseKey(hKey);
+        return 1;
+    }
+
+    wchar_t* ptr = buffer;
+    while (true)
+    {
+        wchar_t* nextPtr = wcsstr(ptr, L";");
+        if (nextPtr == NULL && ptr)
+        {
+            AddDllDirectoryWithEnv(ptr);
+            break;
+        }
+
+        nextPtr[0] = L'\x00';
+        if (!AddDllDirectoryWithEnv(ptr))
+            _plugin_logprintf("AddDllDirectory failed. Directory: '%ls'. Error = 0x%X\n", ptr, GetLastError());
+
+        ptr = nextPtr + 1;
+    }
+
+    // Close the registry key
+    RegCloseKey(hKey);
+
+    // SetDllDirectory(L"asdf");
     return true;
 }
 
